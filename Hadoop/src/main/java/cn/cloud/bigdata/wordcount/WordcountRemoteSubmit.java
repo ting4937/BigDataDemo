d39a864e1633e7fdd0800f877384204cabe375ae
Cloud <huating558@163.com>
2019-05-03 17:15:35 +0800

    mapreduce提交运行

--- Hadoop/src/main/java/cn/cloud/bigdata/wordcount/WordcountRemoteSubmit.java
+++ Hadoop/src/main/java/cn/cloud/bigdata/wordcount/WordcountRemoteSubmit.java
@@ -20,15 +20,27 @@ import java.net.URI;
  * 相当于一个yarn集群的客户端
  * 需要在此封装我们的mr程序的相关运行参数，指定jar包
  * 最后提交给yarn
- * 这是本地运行案例
+ * 这是远程提交到yarn集群案例
  */
-public class WordcountDriver {
+public class WordcountRemoteSubmit {
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
+        //跑在hadoop集群机器上可以不用设置
+        // 在代码中设置JVM系统参数，用于给job对象来获取访问HDFS的用户身份
+        System.setProperty("HADOOP_USER_NAME", "root");
+        // 1、设置job运行时要访问的默认文件系统
+        conf.set("fs.defaultFS", "hdfs://cloudserver:9000");
+        // 2、设置job提交到哪去运行
+        conf.set("mapreduce.framework.name", "yarn");
+        conf.set("yarn.resourcemanager.hostname", "cloudserver");
+        // 3、如果要从windows系统上运行这个job提交客户端程序，则需要加这个跨平台提交的参数
+        conf.set("mapreduce.app-submission.cross-platform","true");
         Job job = Job.getInstance(conf);
 
         //指定本程序的jar包所在的本地路径
-        job.setJarByClass(WordcountDriver.class);
+        //job.setJarByClass(WordcountRemoteSubmit.class);
+        //本地调试需指定jar包
+        job.setJar("E:\\03bigdata_WorkSpace\\BigDataDemo\\Hadoop\\target\\Hadoop-1.0-SNAPSHOT.jar");
         //指定本业务job使用的mapper业务类
         job.setMapperClass(WordcountMapper.class);
         //指定本业务job使用的reduce业务类
@@ -40,12 +52,16 @@ public class WordcountDriver {
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
 
-        //指定job输入目录
-        //运行在windows环境可以写windows路径
-        //运行在hadoop集群写hdfs路径
-        FileInputFormat.setInputPaths(job, new Path("F:\\01_data\\hadoop\\input"));
-        //指定job输出结果所在目录
-        FileOutputFormat.setOutputPath(job, new Path("F:\\01_data\\hadoop\\output"));
+        //指定job输入文件或目录
+        FileInputFormat.setInputPaths(job, new Path("/wordcount/input"));
+        FileSystem fs = FileSystem.get(new URI("hdfs://cloudserver:9000"), conf, "root");
+        Path output = new Path("/wordcount/output");
+        if(fs.exists(output)) {
+            fs.delete(output, true);
+        }
+        fs.close();
+        //指定job输出结果所在目录,输出路径必须不存在
+        FileOutputFormat.setOutputPath(job, output);
 
         //将job中配置的相关参数以及jar包提交给yarn去运行
         //job.submit();
